cc6d6d5661603a69a24c1a3daa3f3be214b2d0af
Sarahranjitkumar <srinalinikutty@gmail.com>
2024-06-02 20:14:55 +0530

    First Commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+console.log("welcome to javascript");
+
+//number examples 
+var num1 = 10;
+var num2 = 20;
+console.log("The value of num1 is - " + num1);
+console.log("The value of num2 is -" + num2);
+
+//String examples
+var fname = "kriti";
+var lname = "sarah";
+console.log("First name is " + fname);
+console.log("Last name is " + lname);
+
